748cc704e1082d48e12b2ae914ada51fdf81304e
wyao905 <warrenyca905@gmail.com>
2020-02-06 14:15:33 -0500

    added index and show actions to units controller

--- delivery-management-api/app/controllers/units_controller.rb
+++ delivery-management-api/app/controllers/units_controller.rb
@@ -1,2 +1,11 @@
 class UnitsController < ApplicationController
+    def index
+        units = Unit.all
+        render json: UnitSerializer.new(units).to_serialized_json
+    end
+
+    def show
+        unit = Unit.find_by(id: params[:id])
+        render json: UnitSerializer.new(unit).to_serialized_json
+    end
 end
